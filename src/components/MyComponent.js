b18c0a85b92953e239ba46bbc7907d16cd46068d
tommy-nguyen-CLV <tommy.nguyen.tpv@one-line.com>
2024-08-20 16:11:23 +0700

    video17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,11 +2,15 @@
 // function component
 import React from "react";
 class MyComponent extends React.Component {
-    //JSX
+    state = {
+        name: 'Tom',
+        address: 'ONE',
+        age: 30
+      }
     render(){
         return (
-            <div> my first component
-                {Math.random()}
+            <div> My name is {this.state.name} 
+            and I'm from {this.state.address}
             </div>
             
         );
