137c01dcfa1d7fc9f62bc951a75012b05228349c
tommy-nguyen-CLV <tommy.nguyen.tpv@one-line.com>
2024-10-18 15:51:44 +0700

    video 24

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,16 +4,22 @@ import React from "react";
 import UserInfor from "./UserInfor";
 import DisplayInfor from "./DisplayInfor";
 class MyComponent extends React.Component {
-
+    state = {
+        listUsers: [
+            {id: 1, name: 'Tom', age: '30'},
+            {id: 2, name: 'Andy', age: '27'},
+            {id: 3, name: 'Joli', age: '22'}
+        ]
+    }
     render(){
-        const myInfor = ['ab', 'c', 'd'];
+        //DRY: don't repeat yourself
         return (
             <div> 
                 <UserInfor/>
                 <br/><br/>
-                <DisplayInfor name='Tommy' age='39'/>
-                <hr/>
-                <DisplayInfor name={'Guffy'} age={21} myInfor={myInfor} />
+                <DisplayInfor 
+                listUsers={this.state.listUsers} 
+                />
             </div>
             
         );
