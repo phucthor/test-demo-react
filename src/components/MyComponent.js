ae686202cf427931b1b0e4bc0671e9630271cc72
tommy-nguyen-CLV <tommy.nguyen.tpv@one-line.com>
2024-10-18 11:23:06 +0700

    video 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,10 +7,21 @@ class MyComponent extends React.Component {
         address: 'ONE',
         age: 30
       }
+
+    handleClick(event) {
+        console.log(">> Click me my button");
+        console.log("My name is ", this.state.name);
+    }
+
+    handleOnMouseOver(event) {
+        console.log(event.pageX);
+    }
+
     render(){
         return (
-            <div> My name is {this.state.name} 
-            and I'm from {this.state.address}
+            <div> My name is {this.state.name} and I'm from {this.state.address}
+            <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+            <button onClick={this.handleClick}>Click me</button>
             </div>
             
         );
