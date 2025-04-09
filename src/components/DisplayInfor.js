a8166b6d36e644b14ff0dfb0a40fd1c052e93fc6
dev-tom <phucthor369@gmail.com>
2025-04-09 17:39:09 +0700

    video 28

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './DisplayInfor.scss';
+import logo from '../logo.svg';
 class DisplayInfor extends React.Component {
     
     state = {
@@ -22,6 +23,7 @@ class DisplayInfor extends React.Component {
     return (
       <div>
         <div className='display-infor-container'>
+            <img src={logo} alt="logo" className="logo" />
             <span onClick={() => {this.handleShowHide()}}>
                 {this.state.isShowListUser === true ? 'Hide User List' : 'Show User List'}</span>
         </div>
@@ -32,7 +34,7 @@ class DisplayInfor extends React.Component {
 
                 return (
                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                    <div style={{ color:'pink', paddingTop: '50px'}}>My name's {user.name}</div>
+                    <div>My name's {user.name}</div>
                     <div>My age's {user.age} </div>
                     <hr />
                 </div>
