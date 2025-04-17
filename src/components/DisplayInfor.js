2af42c5812566aa54c6b37f4f0f45a39a3574f18
dev-tom <phucthor369@gmail.com>
2025-04-17 14:46:57 +0700

    video 38

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,63 +1,34 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "../logo.svg";
+import { use } from "react";
 
 // stateless, stateful
-// class DisplayInfor extends React.Component {
-
-//   render() {
-//     console.log(">>> call render: 3");
-//     //destructuring array/object
-//     const { listUsers } = this.props; //object
-//     // console.log(listUsers);
-//     // console.table(listUsers);
-//     //const listUser = this.props.listUsers;
-
-//     return (
-//       <div>
-//         <div className="display-infor-container">
-//         </div>
-//         {true && (
-//           <>
-//             {listUsers.map((user, index) => {
-//               // console.log(">>> check map user: ", user);
-
-//               return (
-//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                   <div>
-//                     <div>My name's {user.name}</div>
-//                     <div>My age's {user.age} </div>
-//                   </div>
-//                   <div>
-//                     <button
-//                       onClick={() => this.props.handleDeleteUser(user.id)}
-//                     >
-//                       Delete
-//                     </button>
-//                   </div>
-//                 </div>
-//               );
-//             })}
-//           </>
-//         )}
-//       </div>
-//     );
-//   }
-// }
 
 const DisplayInfor = (props) => {
   const { listUsers } = props; //object
   const [isShowHideListUser, setShowHideListUser] = useState(true);
-  
+
   const handleShowHideListUser = () => {
     setShowHideListUser(!isShowHideListUser);
-  }
+  };
+
+  console.log(">>> call render: 3");
+
+  useEffect(() => {
+    if (listUsers.length === 0) {
+      alert("You deleted all users!");
+    }
+    console.log(">>> call render: usseEffect: 1");
+  }, [listUsers]);
+
   return (
     <div>
       <div className="display-infor-container"></div>
       <div>
         <span onClick={() => handleShowHideListUser()}>
-          {isShowHideListUser === true ? "Hide List User" : "Show List User"}</span>
+          {isShowHideListUser === true ? "Hide List User" : "Show List User"}
+        </span>
       </div>
       {isShowHideListUser && (
         <>
