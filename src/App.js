09dc2614156406e9905a397998cfff181901e975
dev-tom <phucthor369@gmail.com>
2025-06-10 01:46:52 +0700

    video 47

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Header from "./components/Header/Header";
 
 const App = () => {
-
   return (
     <div className="app-container">
-      Hello World!
-      <button className="btn btn-primary">Click Me</button>
+      <Header />
     </div>
   );
 };
