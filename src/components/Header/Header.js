68c00edb219b3f260ba1ece5615e794390d51bf4
dev-tom <phucthor369@gmail.com>
2025-07-02 14:35:58 +0700

    video 53.1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,25 +13,21 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <NavLink to="/" className="nav-link">
-                Home
-              </NavLink>
-              <NavLink to="/users" className="nav-link">
-                User
-              </NavLink>
-              <NavLink to="/admins" className="nav-link">
-                Admin
-              </NavLink>
+              <NavLink to="/" className="nav-link">Home</NavLink>
+              <NavLink to="/users" className="nav-link">User</NavLink>
+              <NavLink to="/admins" className="nav-link">Admin</NavLink>
               {/* <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/users">User</Nav.Link>
               <Nav.Link href="/admins">Admin</Nav.Link> */}
             </Nav>
             <Nav>
-              <NavDropdown title="Settings" id="basic-nav-dropdown">
+              <button className='btn-login'>Log in</button>
+              <button className='btn-signup'>Sign up</button>
+              {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
                 <NavDropdown.Item>Log in</NavDropdown.Item>
                 <NavDropdown.Item>Log out</NavDropdown.Item>
                 <NavDropdown.Item>Profile</NavDropdown.Item>
-              </NavDropdown>
+              </NavDropdown> */}
             </Nav>
           </Navbar.Collapse>
         </Container>
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -5,6 +5,13 @@ const HomePage = (props) => {
         <video autoPlay muted loop> 
         <source src={videoHomepage}/>
         </video>
+        <div className="homepage-content">
+            <div className='title-1'>Get to know your customers with forms worth filling out</div>
+            <div className='title-2'>Collect all the data you need to understand customers with forms designed to be refreshingly different.</div>
+            <div className='title-3'>
+                <button className="btn btn-primary">Get Started</button>
+            </div>
+        </div>
     </div>
   );
 }
