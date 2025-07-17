f3c0bb9906cc48e4552fc916664371a04b5ed501
dev-tom <phucthor369@gmail.com>
2025-07-17 11:04:17 +0700

    video 54

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,6 +1,21 @@
+import SideBar from "./SideBar";
+import './Admin.scss';
+import { FaBars } from 'react-icons/fa';
+import React, { useState } from 'react';
 const Admin = (props) => {
+    const [collapsed, setCollapsed] = useState(false);
+    // const [toggled, setToggled] = props.toggled;
     return (
-        <div>Admin component</div>
+        <div className="admin-container">
+            <div className="admin-sidebar">
+                <SideBar collapsed={collapsed}/>
+            </div>
+            <div className="admin-content">
+                <FaBars onClick={()=> setCollapsed(!collapsed)}/>
+                <h1>Admin Page</h1>
+                <p>This is the admin page where you can manage users, settings, and other administrative tasks.</p>
+            </div>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -22,8 +22,8 @@ root.render(
 
           <Route index element={<HomePage />} />
           <Route path="/users" element={<User />} />
-          <Route path="/admins" element={<Admin />} />
         </Route>
+          <Route path="admins" element={<Admin />} />
       </Routes>
     </BrowserRouter>
     {/* </React.StrictMode> */}
