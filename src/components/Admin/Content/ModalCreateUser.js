88f76f477c5d3a007e22df99847461ee9f409af6
dev-tom <phucthor369@gmail.com>
2025-07-23 10:59:56 +0700

    video 57

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -1,24 +1,38 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-
+import { FaFolderPlus } from "react-icons/fa";
 const ModalCreateUser = () => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [role, setRole] = useState("USER");
+  const [image, setImage] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
+  const handleUploadImage = (e) => {
+    if (e.target && e.target.files && e.target.files[0]) {
+      setPreviewImage(URL.createObjectURL(e.target.files[0]));
+      setImage(e.target.files[0]);
+      console.log("handleUploadImage", e.target.files[0]);
+    }
+  }
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
         Launch demo modal
       </Button>
 
-      <Modal 
-        show={show} 
-        onHide={handleClose} 
+      <Modal
+        show={show}
+        onHide={handleClose}
         size="xl"
         backdrop="static"
+        className="modal-add-user"
       >
         <Modal.Header closeButton>
           <Modal.Title>Add New User</Modal.Title>
@@ -27,28 +41,64 @@ const ModalCreateUser = () => {
           <form className="row g-3">
             <div className="col-md-6">
               <label className="form-label">Email</label>
-              <input type="email" className="form-control" />
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="col-md-6">
               <label className="form-label">Password</label>
-              <input type="password" className="form-control" />
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
             <div className="col-md-6">
               <label className="form-label">Username</label>
-              <input type="text" className="form-control" />
+              <input
+                type="text"
+                className="form-control"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
             <div className="col-md-4">
               <label className="form-label">Role</label>
-              <select className="form-select">
-                <option selected value="USER">
-                  USER
-                </option>
+              <select className="form-select" 
+                      onChange={(e) => setRole(e.target.value)} 
+                      value={role}>
+                <option value="USER">USER</option>
                 <option value="ADMIN">ADMIN</option>
               </select>
             </div>
             <div className="col-md-12">
-              <label className="form-label">Image</label>
-              <input type="file" className="form-control" />
+              <label className="form-label label-upload" htmlFor="label-upload">
+                <FaFolderPlus />
+                Upload File Image
+              </label>
+              <input 
+                type="file" 
+                id="label-upload" 
+                hidden
+                onChange={(e) => handleUploadImage(e)}
+              />
+            </div>
+            <div className="col-md-12 img-preview">
+              {previewImage ? 
+              (
+                <img src={previewImage} alt="Preview" />
+              )
+              : (
+                <span>Preview Image</span>
+              )}
+              {/* <img
+                src="https://lh3.googleusercontent.com/pw/AP1GczNIlwEYH14XH4JvPiHTfiliDzb_RYBOwl7GXIUe7fatBYZB8pSYPMqVi4b66KTFHZ31irlkrxy5I8e-DuurwqjQHwX99Q7GKPm_5LqIsrdwoVir7iVEASIQ28jMQFI348KZuLvBvc1-okwdR5BQs8LW=w1086-h1229-s-no-gm?authuser=0"
+                alt="Preview"
+              /> */}
             </div>
           </form>
         </Modal.Body>
@@ -63,6 +113,6 @@ const ModalCreateUser = () => {
       </Modal>
     </>
   );
-}
+};
 
 export default ModalCreateUser;
