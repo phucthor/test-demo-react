e081f42047c05de59348b2857afb0379169bd25a
dev-tom <phucthor369@gmail.com>
2025-09-06 20:20:23 +0700

    video 67

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -103,6 +103,7 @@ const ManageUser = (props) => {
           show={showModalDeleteUser}
           setShow={setShowModalDeleteUser}
           dataDelete={dataDelete}
+          fetchListUsers={fetchListUsers}
         />
       </div>
     </div>
