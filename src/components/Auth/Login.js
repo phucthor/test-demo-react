9640847431cf81b1f00e7503d798fba3f9801d01
dev-tom <phucthor369@gmail.com>
2025-09-26 17:09:58 +0700

    video 78

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -3,10 +3,13 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { postLogin } from "../../services/apiServices";
 import { toast } from 'react-toastify';
+import { useDispatch } from "react-redux";
+import { doLogin } from "../../redux/action/userAction";
 const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const validateEmail = (email) => {
     return String(email)
@@ -19,7 +22,7 @@ const Login = (props) => {
   const handleLogin = async() => {
     // validate
     const isValidEmail = validateEmail(email);
-    console.log('CHECKKKKKKKKKKKKKKKK email', isValidEmail);
+    // console.log('CHECKKKKKKKKKKKKKKKK email', isValidEmail);
     if (!isValidEmail) {
       toast.error("Invalid email format");
       return;
@@ -32,6 +35,7 @@ const Login = (props) => {
     // call api
     let data = await postLogin(email, password);
     if (data && data.EC === 0) {
+      dispatch(doLogin(data));
       toast.success(data.EM);
       navigate("/");
     }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
 import rootReducer from './reducer/rootReducer';
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
-  devTools: process.env.NODE_ENV !== 'production',
+  devTools: process.env.NODE_ENV !== 'production'
 });
 
 export default store;
