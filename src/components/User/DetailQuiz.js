237e80b404ad01fae42c5e9e1f026f23f21bd70b
dev-tom <phucthor369@gmail.com>
2025-09-29 16:25:08 +0700

    video 83 - frontend data processing

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getDataQuiz } from '../../services/apiServices';
+import _ from 'lodash';
 const DetailQuiz = (props) => {
     // const { id } = useParams();
     // console.log("check id:", id);
@@ -14,7 +15,32 @@ const DetailQuiz = (props) => {
 
     const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId);
-        console.log("check res questions:", res);
+        // console.log("check res questions:", res);
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                        // Group the elements of Array based on `color` property
+                        .groupBy("id")
+                        // `key` is group's name (id), `value` is the array of objects
+                        .map((value, key) => {
+                            let answers = [];
+                            let questionDescription, image = null;
+                            value.forEach((item, index) => {
+                                if (index === 0) {
+                                    questionDescription = item.description;
+                                    image = item.image;
+                                }
+                                answers.push(item.answers);
+                                // console.log('item answers:', item.answers);
+                                // item.answers = JSON.parse(item.answers);
+                            });
+                            console.log('value:', value, ' key:', key);
+                            
+                            return { questionId: key, answers : answers, questionDescription, image}
+                        })
+                        .value();
+            console.log("check data:", data);
+        }
     }
 
     return (
